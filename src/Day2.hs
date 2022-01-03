845ede31cb98d3f0e90ece9e3ff99463ca84cbda
pehcy <cheeyung.peh@gmail.com>
2022-01-03 21:25:01 +0800

    update: using Enum for pattern matching

--- src/Day2.hs
+++ src/Day2.hs
@@ -7,28 +7,34 @@ main = print . (\(x,y,z) -> x * y) . move' . tuplify . words =<< readFile "day2.
 -- Part 1: answer is 2039256
 -- main = print . uncurry (*) . move . tuplify . words =<< readFile "day2.txt"
 
-tuplify :: [String] -> [(String,Int)]
-tuplify (x1:x2:xs) = aToInt (x1, x2):tuplify xs
+tuplify :: [String] -> [(Direction,Int)]
+tuplify (x1:x2:xs) = encode (x1, x2):tuplify xs
 tuplify (_:_) = error "Odd number of elements"
 tuplify [] = []
 
-aToInt :: (String, String) -> (String, Int)
-aToInt (x,y) = (x, read y :: Int)
-
 data Direction = Forward | Down | Up
                 deriving (Eq, Enum)
+
+toDirection :: String -> Direction
+toDirection x = case x of
+              "forward" -> Forward
+              "up" -> Up
+              "down" -> Down
+
+encode :: (String, String) -> (Direction, Int)
+encode (x,y) = (toDirection x, read y :: Int)
 
 actions = [("forward", 5),("down", 5), ("forward", 8), 
   ("up", 3), ("down", 8), ("forward", 2)] :: [([Char], Int)]
 
 -- Part 2: added aim value to 3-tuple
-move' :: [([Char], Int)] -> (Int, Int, Int)
+move' :: [(Direction, Int)] -> (Int, Int, Int)
 move' [] = (0,0,0)
 move' xs = foldl f (0,0,0) xs
   where f (a,b,c) (x,y)
-          | x == "forward"  = (a + y, b + (c * y), c)
-          | x == "up"       = (a, b, c - y)
-          | x == "down"     = (a, b, c + y)
+          | x == Forward  = (a + y, b + (c * y), c)
+          | x == Up       = (a, b, c - y)
+          | x == Down     = (a, b, c + y)
           | otherwise       = (a, b, c)
 
 {-- Part 1: only depth
@@ -41,3 +47,4 @@ move xs = foldl f (0,0) xs
           | x == "down"     = (a, b + y)
           | otherwise       = (a, b)
 --}
+
