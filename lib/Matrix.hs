7fc4c7fc8e97050151d4b5c6b031dc3470ddae02
pehcy <cheeyung.peh@gmail.com>
2022-04-20 01:00:56 +0800

    Update Module for Day4

--- lib/Matrix.hs
+++ lib/Matrix.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Matrix (
     Matrix(..),
+    Coord(..),
+    Bingo(..),
+    getFinalScore,
     getRow,
     getCol
     ) where
@@ -9,6 +14,19 @@ newtype Matrix a = Matrix [[a]]
 
 instance Functor Matrix where
     fmap f (Matrix xss) = Matrix (map (map f) xss)
+
+data Coord = Coord { x :: Int, y:: Int } deriving (Eq, Show)
+
+data Bingo = Bingo {
+                nthSet :: Int,
+                lastCalled :: Int,
+                marked :: [Int]
+             } deriving (Read, Show)
+
+
+getFinalScore (Bingo s_ c_ m_) guess mat = let 
+    sumOfUnmarked = sum [x | x <- concat (mat !! s_), x `notElem` m_]
+    in sumOfUnmarked * c_
 
 -- get Row, Column Vector
 getRow, getCol :: Matrix a -> Int -> [a]
