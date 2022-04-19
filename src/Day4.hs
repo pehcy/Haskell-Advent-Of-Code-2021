c12220d8fd37b369d4fe55ffa2f27625b1b8380c
pehcy <cheeyung.peh@gmail.com>
2022-04-20 00:08:23 +0800

    Update Day 4: change method for checking Bingo

--- src/Day4.hs
+++ src/Day4.hs
@@ -4,32 +4,33 @@ module Day4 where
 import Matrix ( Matrix(..), Coord(..) )
 import Data.Function (on)
 import Data.List (groupBy, unfoldr, group, elemIndex)
+import Data.Maybe (mapMaybe)
 import Control.Applicative (Applicative(liftA2))
-import Control.Monad (unless)
-
--- stop until certail condition, same as "until" in Prelude
-converge :: (a -> Maybe a) -> a -> a
-converge f x  = maybe x (converge f) (f x)
 
 (.:^) :: (Functor f, Functor g) => (a -> b) -> g (f a) -> g (f b)
 (.:^) = fmap . fmap
-
--- getBingo input board2d = until (\x -> checkMatches input x) (checkMatches input board2d)
 
 merge :: [a] -> [a] -> [a]
 merge ls [] = ls
 merge [] rs = rs
 merge (x:xs) (y:ys) = x : y : merge xs ys
 
+{-
+The `elem` function only tell us whether some element is 
+part of a list, but not where it is. So the result is a `Maybe` monad.
+Here I import the standard library to map Monad into the final result.
+-}
 -- getBingo :: (Foldable t, Eq a, Functor g) => t a -> g [[a]] -> g [(Int, Int)]
 checkBingo guess boards = let
         matches = checkMatches guess <$> boards
         xlist = f' (x .:^ matches)
         ylist = f' (y .:^ matches)
-        in xlist `merge` ylist
-        where   f' xs = filter (any ((== 5) . snd))
-                        ((\x -> (head x, length x))
-                        .:^ (group <$> xs))
+        xAt = if g' xlist `setEq` [] then [] else mapMaybe (`elemIndex` xlist) (g' xlist)
+        yAt = if g' ylist `setEq` [] then [] else mapMaybe (`elemIndex` ylist) (g' ylist)
+        in xAt `merge` yAt
+        where   f' xs = (\x -> (head x, length x))
+                        .:^ (group <$> xs)
+                g' ls = filter (any ((== 5) . snd)) ls
 
 -- Iterate loop until a condition is met
 takeUntil :: Foldable t => (a -> Bool) -> t a -> [a]
@@ -41,12 +42,12 @@ main = do
   let content = lines fileLines
   let guess = num2Int <$> wordsWhen ((==',')<||>(==' ')) (head content)
   let boards = toMatrix $ fmap words <$> sep content
-  let g = takeUntil (\x -> not (checkBingo (take x guess) boards `setEq` [])) [0..]
+  let num = last $ takeUntil (\x -> not (checkBingo (take x guess) boards `setEq` [])) [0..]
   -- let list = head 
   --     $ dropWhile (setEq []) 
   --      $ flip checkBingo boards 
   --      . flip take guess <$> [1..]
-  return g
+  return (checkBingo (take 18 guess) boards)
 
 enumerate :: [b] -> [(Int, b)]
 enumerate = zip [0..]
@@ -60,6 +61,13 @@ checkMatches drawn tiles = [
 num2Int :: String -> Int
 num2Int xs = read xs :: Int
 
+{---------------------------------------------------------------
+fold reduces a list into a single value, 
+while unfold generates a list from a given seed value
+-}
+--chunk :: Int -> [a] -> [[a]]
+--chunk n = unfoldr (\xs -> if null xs then Nothing else Just (splitAt n xs))
+----------------------------------------------------------------}
 sep :: [a] -> [[a]]
 sep xs = fmap tail (chunk 6 $ drop 1 xs)
         where chunk n = unfoldr (\xs -> if null xs then Nothing else Just (splitAt n xs))
@@ -91,13 +99,6 @@ wordsWhen p s = case dropWhile p s of
 
 toMatrix :: [[[String]]] -> [[[Int]]]
 toMatrix = (fmap . fmap . fmap) num2Int
-{---------------------------------------------------------------
-fold reduces a list into a single value, 
-while unfold generates a list from a given seed value
--}
---chunk :: Int -> [a] -> [[a]]
---chunk n = unfoldr (\xs -> if null xs then Nothing else Just (splitAt n xs))
-----------------------------------------------------------------}
 
 -- sample inputs --
 list :: [Int]
